6c2136b98c93e0a643f663033a367a0f1309933c
joaoaluz <joao_aluz@hotmail.com>
2016-09-05 23:32:47 -0300

    Usar em pt-br

--- testedeteccaodeimagem.m
+++ testedeteccaodeimagem.m
@@ -15,6 +15,6 @@ imshow(I); hold on
 for i = 1:size(BB,1)
     rectangle('Position',BB(i,:),'LineWidth',5,'LineStyle','-','EdgeColor','r');
 end
-title(' Facial Detection');
+title(' Facial Detecção');
 hold off;
 
